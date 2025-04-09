d9fb3783d7a91b774b720843334c3905ba26034e
IgorKawalec <251166@edu.p.lodz.pl>
2025-04-09 19:39:09 +0200

    6. zmiana literalow na stale

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,31 +2,34 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    public static final int CAPACITY = 12;
+    public static final int EMPTY_VALUE = -1;
+    public static final int DEFAULT_VALUE = -1;
+    private int[] numbers = new int[CAPACITY];
 
     public int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++total] = in;
+            numbers[++total] = in;
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY_VALUE;
     }
         
     public boolean isFull() {
-        return total == 11;
+        return total == CAPACITY - 1;
     }
         
     protected int peekaboo() {
-        if (callCheck()) return -1;
-        return NUMBERS[total];
+        if (callCheck()) return EMPTY_VALUE;
+        return numbers[total];
     }
             
     public int countOut() {
-        if (callCheck()) return -1;
-        return NUMBERS[total--];
+        if (callCheck()) return EMPTY_VALUE;
+        return numbers[total--];
     }
 
 }
