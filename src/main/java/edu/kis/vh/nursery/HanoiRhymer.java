8d2b61c1225ae74cb69cd9682caf8321e186a6e0
IgorKawalec <251166@edu.p.lodz.pl>
2025-06-14 21:15:28 +0200

    Poprawka punktu 2. - dopasowanie wcięć

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -8,12 +8,10 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
         return totalRejected;
     }
 
-    @Override
     public void countIn(int in) {
-        if (!callCheck() && in > peekaboo()) {
+        if (!callCheck() && in > peekaboo())
             totalRejected++;
-        } else {
+        else
             super.countIn(in);
-        }
     }
 }
